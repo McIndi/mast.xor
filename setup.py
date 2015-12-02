d6a88718e268ff7f32e6787deb8d28a1f71b3344
ilovetux <me@ilovetux.com>
2015-12-02 12:05:47 -0500

    Version Bump

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def read(fname):
 
 setup(
     name = "mast.xor",
-    version = "2.0.0",
+    version = "2.1.0",
     author = "Clifford Bressette",
     author_email = "cliffordbressette@mcindi.com",
     description = ("A library which allows you to xor and base64 encode/decode strings"),
